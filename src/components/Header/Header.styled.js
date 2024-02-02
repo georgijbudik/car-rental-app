aacd4f869f8f2dd63082e39fae31e6d36ba0a4fa
georgijbudik <georgijbudik@gmail.com>
2024-02-02 14:22:21 +0100

    add header styles

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -3,4 +3,8 @@ import styled from 'styled-components';
 export const StyledHeader = styled.header`
   width: 100%;
   height: 50px;
+
+  display: flex;
+  align-items: center;
+  padding: 16px 0 48px 0;
 `;

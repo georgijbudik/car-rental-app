a16fa66586b1490cb649902e4fc6bbf80b890217
georgijbudik <georgijbudik@gmail.com>
2024-02-02 14:21:40 +0100

    add header nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,12 +1,18 @@
-import { StyledLink } from './Nav.styled';
+import { NavList, StyledLink } from './Nav.styled';
 
 const Nav = () => {
   return (
-    <>
-      <StyledLink to="/">Home</StyledLink>
-      <StyledLink to="catalog">Catalog</StyledLink>
-      <StyledLink to="favorites">Favorites</StyledLink>
-    </>
+    <NavList>
+      <li>
+        <StyledLink to="/">Home</StyledLink>
+      </li>
+      <li>
+        <StyledLink to="catalog">Catalog</StyledLink>
+      </li>
+      <li>
+        <StyledLink to="favorites">Favorites</StyledLink>
+      </li>
+    </NavList>
   );
 };
 
--- src/components/Nav/Nav.styled.js
+++ src/components/Nav/Nav.styled.js
@@ -2,7 +2,18 @@ import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const StyledLink = styled(NavLink)`
-  padding: 12px;
-  background-color: var(--accent-color);
-  color: white;
+  font-size: 18px;
+  padding: 12px 26px;
+  background-color: var(--button-color);
+  color: var(--white-color);
+  border-radius: 8px;
+  &:hover {
+    background-color: var(--accent-color);
+  }
+`;
+
+export const NavList = styled.ul`
+  display: flex;
+  gap: 16px;
+  margin: 0 auto;
 `;

7abd31bcc99a020a3ecb354e42dc3f4424a87810
georgijbudik <georgijbudik@gmail.com>
2024-02-02 14:21:19 +0100

    add catalog page

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,5 +1,28 @@
+import Filters from 'components/Filters/Filters';
+import { CatalogContainer } from './Catalog.styled';
+import { StyledTitle } from 'components/Title/Title.styled';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchCatalog } from 'redux_/catalog/catalogOperations';
+import CardList from 'components/CardList/CardList';
+
 const Catalog = () => {
-  return <div>Catalog</div>;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCatalog());
+  }, [dispatch]);
+
+  return (
+    <>
+      <div style={{ display: 'flex' }}>
+        <StyledTitle>Our catalog</StyledTitle>
+      </div>
+      <CatalogContainer>
+        <Filters />
+      </CatalogContainer>
+      <CardList />
+    </>
+  );
 };
 
 export default Catalog;
---
+++ src/pages/Catalog.styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const CatalogContainer = styled.div`
+  display: flex;
+  margin: 0 auto;
+`;

008b143cbf78894de3d9cba170a5e1d90805f7de
georgijbudik <georgijbudik@gmail.com>
2024-02-04 00:14:54 +0100

    add not found image

--- src/components/Title/Title.styled.js
+++ src/components/Title/Title.styled.js
@@ -4,4 +4,5 @@ export const StyledTitle = styled.h1`
   font-size: 42px;
   margin: 0 auto;
   margin-bottom: 48px;
+  padding-top: 24px;
 `;
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -5,17 +5,20 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchCatalog, fetchMakes } from 'redux_/catalog/catalogOperations';
 import CardList from 'components/CardList/CardList';
 import Title from 'components/Title/Title';
-import { selectIsLoading } from 'redux_/catalog/catalogSelectors';
+import {
+  selectCatalog,
+  selectIsLoading,
+} from 'redux_/catalog/catalogSelectors';
 import Loader from 'components/Loader/Loader';
 
 const Catalog = () => {
   const [page, setPage] = useState(1);
   const isLoading = useSelector(selectIsLoading);
-
-  console.log(page);
+  const catalog = useSelector(selectCatalog);
 
   const onLoadMore = () => {
-    dispatch(fetchCatalog(page + 1));
+    setPage(page + 1);
+    dispatch(fetchCatalog({ page }));
   };
 
   const dispatch = useDispatch();
@@ -35,11 +38,28 @@ const Catalog = () => {
       {isLoading ? (
         <Loader />
       ) : (
-        <div>
-          <CardList />
-          <LoadMoreButton type="button" onClick={onLoadMore} hidden={isLoading}>
-            Load more
-          </LoadMoreButton>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            paddingBottom: '40px',
+          }}
+        >
+          {catalog.length === 0 ? (
+            <div>The list is empty</div>
+          ) : (
+            <>
+              <CardList />
+              <LoadMoreButton
+                type="button"
+                onClick={onLoadMore}
+                hidden={isLoading}
+              >
+                Load more
+              </LoadMoreButton>
+            </>
+          )}
         </div>
       )}
     </>

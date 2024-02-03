fe861a6ddea6d6a8b581054fb44baea3ab075974
georgijbudik <georgijbudik@gmail.com>
2024-02-03 20:08:49 +0100

    add favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,5 +1,30 @@
+import CardItem from 'components/CardItem/CardItem';
+import CardList from 'components/CardList/CardList';
+import { StyledItem, StyledList } from 'components/CardList/CardList.styled';
+import Title from 'components/Title/Title';
+import { useSelector } from 'react-redux';
+import { selectFavorites } from 'redux_/favorites/favoritesSelectors';
+
 const Favorites = () => {
-  return <div>Favorites</div>;
+  const favorites = useSelector(selectFavorites);
+  return (
+    <div>
+      <div style={{ display: 'flex' }}>
+        <Title>Your favorites</Title>
+      </div>
+      {favorites.length > 0 ? (
+        <StyledList>
+          {favorites.map(item => (
+            <StyledItem key={item.id}>
+              <CardItem item={item} />
+            </StyledItem>
+          ))}
+        </StyledList>
+      ) : (
+        <div>No favorites</div>
+      )}
+    </div>
+  );
 };
 
 export default Favorites;

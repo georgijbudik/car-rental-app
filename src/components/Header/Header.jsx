9140578f81d4690367e6a1c0da271ef865707dcd
georgijbudik <georgijbudik@gmail.com>
2024-02-04 00:12:15 +0100

    header logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,18 @@
 import Nav from 'components/Nav/Nav';
-import { StyledHeader } from './Header.styled';
+import { Logo, LogoTitle, LogoWrap, StyledHeader } from './Header.styled';
+import myGif from '../../images/icon-logo-gif.gif';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <StyledHeader>
+      <LogoWrap>
+        <Link to={'/'}>
+          <Logo src={myGif} alt="Car" />
+        </Link>
+
+        <LogoTitle>Car Rental</LogoTitle>
+      </LogoWrap>
       <Nav />
     </StyledHeader>
   );
--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -1,10 +1,30 @@
 import styled from '@emotion/styled';
 
 export const StyledHeader = styled.header`
-  width: 100%;
   height: 50px;
+  border-radius: 4px;
 
   display: flex;
+
   align-items: center;
-  padding: 16px 0 48px 0;
+  padding: 16px 205px;
+  background-color: white;
+`;
+
+export const Logo = styled.img`
+  width: 40px;
+  height: 40px;
+  margin-right: 8px;
+`;
+
+export const LogoWrap = styled.div`
+  display: flex;
+  padding-right: 240px;
+  align-items: center;
+`;
+
+export const LogoTitle = styled.p`
+  font-weight: 700;
+  font-size: 24px;
+  color: var(--button-color);
 `;

40b0be4ae8404ac55172afc203c8ece2c84b439b
georgijbudik <georgijbudik@gmail.com>
2024-02-04 21:59:51 +0100

    add tostify

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from 'redux_/store';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="car-rental-app">
     <Provider store={store}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      <App />
     </Provider>
+    <ToastContainer />
   </BrowserRouter>
 );
